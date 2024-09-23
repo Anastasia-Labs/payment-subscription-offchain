86bacc48dc02b1cc7b9622acb3508ffccf988c17
Harun Mwangi <harunm28@gmail.com>
2024-09-23 14:34:30 +0300

    refactor(validators.ts): made the code more concise

--- test/compiled/validators.ts
+++ test/compiled/validators.ts
@@ -5,6 +5,7 @@ import {
 } from "@lucid-evolution/lucid";
 
 import blueprint from "./plutus.json" assert { type: "json" };
+import { Script } from "@lucid-evolution/lucid";
 
 export type Validators = {
     spendService: SpendingValidator;
@@ -14,54 +15,19 @@ export type Validators = {
 };
 
 export function readMultiValidators(): Validators {
-    const spendService = blueprint.validators.find((v) =>
-        v.title === "service_multi_validator.spend_service"
-    );
-
-    if (!spendService) {
-        throw new Error("spendService validator not found");
-    }
-
-    const mintService = blueprint.validators.find((v) =>
-        v.title === "service_multi_validator.mint_service"
-    );
-
-    if (!mintService) {
-        throw new Error("Mint NFT validator not found");
-    }
-
-    const spendAccount = blueprint.validators.find((v) =>
-        v.title === "account_multi_validator.spend_account"
-    );
-
-    if (!spendAccount) {
-        throw new Error("spendAccount validator not found");
-    }
-
-    const mintAccount = blueprint.validators.find((v) =>
-        v.title === "account_multi_validator.mint_account"
-    );
-
-    if (!mintAccount) {
-        throw new Error("Mint NFT validator not found");
-    }
+    const getValidator = (title: string): Script => {
+        const validator = blueprint.validators.find((v) => v.title === title);
+        if (!validator) throw new Error(`Validator not found: ${title}`);
+        return {
+            type: "PlutusV2",
+            script: applyDoubleCborEncoding(validator.compiledCode),
+        };
+    };
 
     return {
-        spendService: {
-            type: "PlutusV2",
-            script: applyDoubleCborEncoding(spendService.compiledCode),
-        },
-        mintService: {
-            type: "PlutusV2",
-            script: applyDoubleCborEncoding(mintService.compiledCode),
-        },
-        spendAccount: {
-            type: "PlutusV2",
-            script: applyDoubleCborEncoding(spendAccount.compiledCode),
-        },
-        mintAccount: {
-            type: "PlutusV2",
-            script: applyDoubleCborEncoding(mintAccount.compiledCode),
-        },
+        spendService: getValidator("service_multi_validator.spend_service"),
+        mintService: getValidator("service_multi_validator.mint_service"),
+        spendAccount: getValidator("account_multi_validator.spend_account"),
+        mintAccount: getValidator("account_multi_validator.mint_account"),
     };
 }
